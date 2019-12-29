2ff55b31bc3326c1accd717db976edeb02b298fd
Danuel <public.danuel@gmail.com>
2019-12-29 21:29:20 +0900

    change to Apollo-based

--- src/index.local.ts
+++ src/index.local.ts
@@ -1,12 +1,22 @@
-import { printSchema } from "graphql";
+import { ApolloServer } from "apollo-server-fastify";
+import Fastify from "fastify";
 
+import { configs } from "./configs";
+import { createContext } from "./context";
 import { initializeRepositories } from "./repositories";
 import { schema } from "./schema";
 
+const createServer = () => {
+  const apolloServer = new ApolloServer({ schema, playground: true, context: createContext });
+  const server = Fastify();
+  server.register(apolloServer.createHandler({ cors: true }));
+  return server;
+};
+
 const run = async () => {
   await initializeRepositories;
-  console.log(printSchema(schema));
-  process.exit();
+  await createServer().listen(configs.port);
+  console.log("RUNNING");
 };
 
 run();
