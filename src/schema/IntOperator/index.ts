7bdef741a49138b601b2d268b84e53d566359220
Danuel <public.danuel@gmail.com>
2019-12-30 01:24:31 +0900

    add IntOperator

---
+++ src/schema/IntOperator/index.ts
@@ -0,0 +1,51 @@
+import { GraphQLInputObjectType, GraphQLInt, GraphQLList, GraphQLNonNull } from "graphql";
+
+const Scalar = GraphQLInt;
+
+const createOperator = () => ({
+  eq: {
+    type: Scalar
+  },
+  notEq: {
+    type: Scalar
+  },
+  in: {
+    type: new GraphQLList(new GraphQLNonNull(Scalar))
+  },
+  notIn: {
+    type: new GraphQLList(new GraphQLNonNull(Scalar))
+  },
+  gt: {
+    type: Scalar
+  },
+  gte: {
+    type: Scalar
+  },
+  lt: {
+    type: Scalar
+  },
+  lte: {
+    type: Scalar
+  },
+  between: {
+    type: new GraphQLList(new GraphQLNonNull(Scalar))
+  },
+  notBeetween: {
+    type: new GraphQLList(new GraphQLNonNull(Scalar))
+  }
+});
+
+const IntOrOperator = new GraphQLInputObjectType({
+  name: "IntOrOperator",
+  fields: { ...createOperator() }
+});
+
+export const IntOperator = new GraphQLInputObjectType({
+  name: "IntOperator",
+  fields: {
+    ...createOperator(),
+    or: {
+      type: new GraphQLList(new GraphQLNonNull(IntOrOperator))
+    }
+  }
+});
