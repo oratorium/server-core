27faabb5a33b18355ebd8c9831626e79f06e3429
Danuel <public.danuel@gmail.com>
2019-12-30 01:24:25 +0900

    add StringOperator

---
+++ src/schema/StringOperator/index.ts
@@ -0,0 +1,42 @@
+import { GraphQLInputObjectType, GraphQLList, GraphQLNonNull, GraphQLString } from "graphql";
+
+const Scalar = GraphQLString;
+
+const createOperator = () => ({
+  eq: {
+    type: Scalar
+  },
+  notEq: {
+    type: Scalar
+  },
+  in: {
+    type: new GraphQLList(new GraphQLNonNull(Scalar))
+  },
+  notIn: {
+    type: new GraphQLList(new GraphQLNonNull(Scalar))
+  },
+  startsWith: {
+    type: Scalar
+  },
+  endsWith: {
+    type: Scalar
+  },
+  contains: {
+    type: Scalar
+  }
+});
+
+const StringOrOperator = new GraphQLInputObjectType({
+  name: "StringOrOperator",
+  fields: { ...createOperator() }
+});
+
+export const StringOperator = new GraphQLInputObjectType({
+  name: "StringOperator",
+  fields: {
+    ...createOperator(),
+    or: {
+      type: new GraphQLList(new GraphQLNonNull(StringOrOperator))
+    }
+  }
+});
