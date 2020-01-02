c82a7a6b4738ba558459132b52fbe996e2c6a15b
Danuel <public.danuel@gmail.com>
2020-01-02 23:30:19 +0900

    add DocumentLoader, DocumentFragmentLoader

--- src/context/createLoaders/index.ts
+++ src/context/createLoaders/index.ts
@@ -1,11 +1,15 @@
 import DataLoader from "dataloader";
 
+import { DocumentRepository } from "../../repositories/Document";
+import { DocumentFragmentRepository } from "../../repositories/DocumentFragment";
 import { PreferenceRepository } from "../../repositories/Preference";
 import { UserRepository } from "../../repositories/User";
 import { createBatcher } from "./createBatcher";
 import { createQueryRunner } from "./createQueryRunner";
 
 export const createLoaders = () => ({
+  document: new DataLoader(createBatcher(DocumentRepository, item => item.id)),
+  documentFragment: new DataLoader(createBatcher(DocumentFragmentRepository, item => item.id)),
   preference: new DataLoader(createBatcher(PreferenceRepository, item => item.id)),
   query: createQueryRunner(),
   user: new DataLoader(createBatcher(UserRepository, item => item.id))
