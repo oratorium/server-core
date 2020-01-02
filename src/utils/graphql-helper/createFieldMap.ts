b0ed0f5c0abd9a738c598b2a0dc841d29413279a
Danuel <public.danuel@gmail.com>
2020-01-03 02:13:32 +0900

    improve createFieldMap

--- src/utils/graphql-helper/createFieldMap.ts
+++ src/utils/graphql-helper/createFieldMap.ts
@@ -3,5 +3,5 @@ import { GraphQLFieldConfigMap } from "graphql";
 import { Context } from "../../context";
 
 export const createFieldMap = <TSource = any, TArgs = { [argName: string]: any }>(
-  fieldMap: GraphQLFieldConfigMap<TSource, Context, TArgs>
-) => fieldMap as GraphQLFieldConfigMap<any, any, any>;
+  fieldMap: GraphQLFieldConfigMap<TSource, Context, TArgs> | (() => GraphQLFieldConfigMap<TSource, Context, TArgs>)
+) => (typeof fieldMap === "function" ? fieldMap() : fieldMap) as GraphQLFieldConfigMap<any, any, any>;
