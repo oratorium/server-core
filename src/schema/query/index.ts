c71dbb2fcbc67ef20aaeae516699c5bcef0ca1a2
Danuel <public.danuel@gmail.com>
2020-01-03 02:35:20 +0900

    remove DocumentFragment API

--- src/schema/query/index.ts
+++ src/schema/query/index.ts
@@ -5,8 +5,6 @@ import { comment } from "./comment";
 import { comments } from "./comments";
 import { document } from "./document";
 import { documents } from "./documents";
-import { fragment } from "./fragment";
-import { fragments } from "./fragments";
 import { preference } from "./preference";
 import { preferences } from "./preferences";
 import { user } from "./user";
@@ -14,5 +12,5 @@ import { users } from "./users";
 
 export const query = new GraphQLObjectType({
   name: "Query",
-  fields: createFieldMap({ comment, comments, document, documents, fragment, fragments, preference, preferences, user, users })
+  fields: createFieldMap({ comment, comments, document, documents, preference, preferences, user, users })
 });
