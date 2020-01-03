c852d74a8967f1794a68a10c30f6a40297ac7418
Danuel <public.danuel@gmail.com>
2020-01-03 21:41:36 +0900

    remove wrong explicit type

--- src/context/createAuth/index.ts
+++ src/context/createAuth/index.ts
@@ -1,18 +1,12 @@
 import { UserRepository } from "../../repositories/User";
 import { memo } from "../../utils/memo";
 
-type Payload = {
-  user: {
-    id: number;
-  };
-};
-
 export const createAuth = (authorization?: string) =>
   memo(() => {
     if (!authorization) {
       throw new Error("로그인 상태가 아닙니다");
     }
-    const payload = UserRepository.parseToken<Payload>(authorization);
+    const payload = UserRepository.parseToken(authorization);
     if (!payload) {
       throw new Error("로그인 상태가 아닙니다");
     }
